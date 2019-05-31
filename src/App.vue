b1bf196e4faa7bfd601fd66db4c408e34c51783e
serect1107 <2074255835@qq.com>
2019-05-31 10:13:55 +0800

    a

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+		<cnpheader></cnpheader>
+    <router-view/>
+  </div>
+</template>
+<script>
+	import cnpheader from '@/components/header/header'
+	export default {
+		name: 'App',
+		components:{
+			cnpheader
+		}
+	}
+</script>
+<style lang="stylus">
+	#app 
+		font-family: 'Avenir', Helvetica, Arial, sans-serif
+		color: #2c3e50
+	
+</style>
